f80c73187c8016b3e9e7ec0e28b518f633391726
alexelec <alexelecv5@gmail.com>
2017-08-07 09:41:49 +0300

    Init. Amlogic buildroot release 2017-06-30

---
+++ amavutils/include/amconfigutils.h
@@ -0,0 +1,29 @@
+#ifndef FF_CONFIGS_H__
+#define FF_CONFIGS_H__
+
+#define MAX_CONFIG 128
+#define CONFIG_PATH_MAX    32
+#define CONFIG_VALUE_MAX   92
+#define CONFIG_VALUE_OFF   (CONFIG_PATH_MAX+4)
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+    int am_config_init(void);
+    int am_getconfig(const char * path, char *val, const char * def);
+    int am_setconfig(const char * path, const char *val);
+    int am_setconfig_float(const char * path, float value);
+    int am_getconfig_float(const char * path, float *value);
+    int am_dumpallconfigs(void);
+    int am_getconfig_bool(const char * path);
+    int am_getconfig_bool_def(const char * path, int def);
+    int am_getconfig_int_def(const char * path, int def);
+    float am_getconfig_float_def(const char * path, float defvalue);
+#ifndef ANDROID	
+	int property_get(const char *key, char *value, const char *default_value);
+#endif	
+#ifdef  __cplusplus
+}
+#endif
+#endif
+
